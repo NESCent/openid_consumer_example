9aaa13229a3fbe995872f5fe74213cf2ef3ba35e
Dan Leehr <dan.leehr@nescent.org>
2012-12-12 09:22:08 -0500

    More cleanup, removed database connectivity

--- db/migrate/20121119184427_do_nothing_yet.rb
+++
@@ -1,9 +0,0 @@
-class DoNothingYet < ActiveRecord::Migration
-  def up
-  	# instructions for changing database to new state
-  end
-
-  def down
-  	# instructions for changing database to previous state
-  end
-end
